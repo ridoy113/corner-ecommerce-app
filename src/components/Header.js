13a391e44d3eceaad106178d5c001d01f40166bb
ridoy113 <almamounhridoy113@gmail.com>
2023-01-26 12:35:20 +0600

    working start from header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <>
+      <header className="header-top-stripe py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6"></div>
+            <div className="col-6"></div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
 
-export default Header
+export default Header;
