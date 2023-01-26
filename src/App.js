beaaa8ac8a8de5d650087bfe3e1a4dc7e1cbd117
ridoy113 <almamounhridoy113@gmail.com>
2023-01-26 12:15:22 +0600

    add router and google fonts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
 import "./App.css";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return<h1>
-  Hello World
-  </h1>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
