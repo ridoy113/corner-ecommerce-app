cf376c6e1cd8995825ef57753fc2bc87a5025652
ridoy113 <almamounhridoy113@gmail.com>
2023-01-27 10:43:56 +0600

    number add

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,17 @@ import { NavLink, Link } from "react-router-dom";
 const Header = () => {
   return (
     <>
-      <header className="header-top-stripe py-3">
+      <header className="header-top-strip py-3">
         <div className="container-xxl">
           <div className="row">
-            <div className="col-6"></div>
-            <div className="col-6"></div>
+            <div className="col-6">
+              <p>Free Shipping Over $100 & Free Return</p>
+            </div>
+            <div className="col-6">
+              <p className="text-end ">
+                Hotline: <a href="tel:+880 1874997427">+880 1874997427</a>
+              </p>
+            </div>
           </div>
         </div>
       </header>
