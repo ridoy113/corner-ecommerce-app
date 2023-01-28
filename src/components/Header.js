f6127e59c1fe3015397536044233eaa48e3800ac
ridoy113 <almamounhridoy113@gmail.com>
2023-01-28 10:35:36 +0600

    header top complite

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,13 +8,32 @@ const Header = () => {
         <div className="container-xxl">
           <div className="row">
             <div className="col-6">
-              <p>Free Shipping Over $100 & Free Return</p>
-            </div>
-            <div className="col-6">
-              <p className="text-end ">
-                Hotline: <a href="tel:+880 1874997427">+880 1874997427</a>
+              <p className="text-white mb-0">
+                Free Shipping Over $100 & Free Return
               </p>
             </div>
+            <div className="col-6">
+              <p className="text-end text-white mb-0">
+                Hotline:
+                <a className="text-white" href="tel:+880 1874997427">
+                  +880 1874997427
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </header>
+
+      <header className="header-upper py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-2">
+              <h2>
+                <Link className="text-white">Dev Ridoy</Link>
+              </h2>
+            </div>
+            <div className="col-5"></div>
+            <div className="col-5"></div>
           </div>
         </div>
       </header>
