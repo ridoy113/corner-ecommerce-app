8f44659ae08c28d43c68faabaaa6c513a2311a93
ridoy113 <almamounhridoy113@gmail.com>
2023-01-28 10:56:21 +0600

    header input complite

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
+import { BsSearch } from "react-icons/bs";
 
 const Header = () => {
   return (
@@ -26,13 +27,26 @@ const Header = () => {
 
       <header className="header-upper py-3">
         <div className="container-xxl">
-          <div className="row">
+          <div className="row align-items-center">
             <div className="col-2">
               <h2>
                 <Link className="text-white">Dev Ridoy</Link>
               </h2>
             </div>
-            <div className="col-5"></div>
+            <div className="col-5">
+              <div className="input-group">
+                <input
+                  type="text"
+                  className="form-control py-2"
+                  placeholder="Search Product Here..."
+                  aria-label="Search Product Here..."
+                  aria-describedby="basic-addon2"
+                />
+                <span className="input-group-text p-3" id="basic-addon2">
+                  <BsSearch className="fs-6" />
+                </span>
+              </div>
+            </div>
             <div className="col-5"></div>
           </div>
         </div>
