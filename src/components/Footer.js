230e890c3b9f93801efb3d2ddbc9f29c9bd802bc
ridoy113 <almamounhridoy113@gmail.com>
2023-01-29 17:02:01 +0600

    footer ready

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { BsLinkedin, BsGithub, BsYoutube, BsInstagram } from "react-icons/bs";
 
 const Footer = () => {
   return (
@@ -31,7 +33,74 @@ const Footer = () => {
       </footer>
 
       <footer className="py-4">
-        <div className="container-xxl"></div>
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-4">
+              <h4 className="text-white mb-4">Contact Us</h4>
+              <div>
+                <address className="text-white fs-6 ">
+                  Hmo: Abidpur, Bourichong,
+                  <br /> Comilla <br />
+                  PinCode: 018749
+                </address>
+                <a
+                  href="tel:+880 1874997427"
+                  className="mt-3 d-block mb-1 text-white"
+                >
+                  +880 1874997427
+                </a>
+                <a
+                  href="mailto:almamounhridoy113@gmail.com"
+                  className="mt-2 d-block mb-0 text-white"
+                >
+                  almamounhridoy113@gmail.com
+                </a>
+
+                <div className="social_icons d-flex align-items-center gap-30 mt-4">
+                  <a href="">
+                    <BsLinkedin className="text-white fs-4" />
+                  </a>
+                  <a href="">
+                    <BsInstagram className="text-white fs-4" />
+                  </a>
+                  <a href="">
+                    <BsGithub className="text-white fs-4" />
+                  </a>
+                  <a href="">
+                    <BsYoutube className="text-white fs-4" />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-3">
+              <h4 className="text-white mb-4">Information</h4>
+              <div className="footer-links d-flex flex-column">
+                <Link className="text-white py-2 mb-1">Privacy Policy</Link>
+                <Link className="text-white py-2 mb-1">Refund Policy</Link>
+                <Link className="text-white py-2 mb-1">Shipping Policy</Link>
+                <Link className="text-white py-2 mb-1">Terms of Service</Link>
+                <Link className="text-white py-2 mb-1">Blogs</Link>
+              </div>
+            </div>
+            <div className="col-3">
+              <h4 className="text-white mb-4">Account</h4>
+              <div className="footer-links d-flex flex-column">
+                <Link className="text-white py-2 mb-1">About Us</Link>
+                <Link className="text-white py-2 mb-1">Faq</Link>
+                <Link className="text-white py-2 mb-1">Contact</Link>
+              </div>
+            </div>
+            <div className="col-2">
+              <h4 className="text-white mb-4">Quick Links</h4>
+              <div className="footer-links d-flex flex-column">
+                <Link className="text-white py-2 mb-1">Laptops</Link>
+                <Link className="text-white py-2 mb-1">Headphones</Link>
+                <Link className="text-white py-2 mb-1">Tablets</Link>
+                <Link className="text-white py-2 mb-1">Watch</Link>
+              </div>
+            </div>
+          </div>
+        </div>
       </footer>
 
       <footer className="py-4">
