721f5cf715aa35631dfcf2ea3a6dd3df8464235c
ridoy113 <almamounhridoy113@gmail.com>
2023-02-02 00:02:14 +0600

    home section first banner big img add

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
     <>
-    <section className="home-wrapper-1 py-5">
-      <div className="container-xxl">
-        <div className="row">
-          <div className="col-6">
-            <div className="main-banner-content p-3">
-              <img src="images/" alt="" />
+      <section className="home-wrapper-1 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <div className="main-banner-content p-3">
+                <img
+                  src="images/main-banner-1.jpg"
+                  className="img-fluid rounded-3"
+                  alt="main-banner"
+                />
+                <div className="main-banner-content">
+                  
+                </div>
+              </div>
             </div>
+            <div className="col-6"></div>
           </div>
-          <div className="col-6"></div>
         </div>
-      </div>
-    </section>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
